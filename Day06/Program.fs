9193c4c6ec0a7bc6d609b3fb1d0af878ed5cc937
Paul Papathomas <paul@pauls-mbp.home>
2022-12-06 19:37:19 +0000

    Day6 initial commit

---
+++ Day06/Program.fs
@@ -0,0 +1,10 @@
+﻿let input = System.IO.File.ReadAllLines(@"input.txt").[0]
+
+let findMarker nDistinctChars =
+    input
+    |> Seq.windowed nDistinctChars
+    |> Seq.findIndex (fun el -> (Set.ofSeq el).Count = nDistinctChars)
+    |> (+) nDistinctChars
+
+printfn "P1 answer: %i" (findMarker 4)
+printfn "P2 answer: %i" (findMarker 14)
