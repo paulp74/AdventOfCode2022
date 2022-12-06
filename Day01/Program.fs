976c624ce07963328319acbfd6a2920bd97aed98
Paul Papathomas <paul@pauls-mbp.home>
2022-12-06 19:54:43 +0000

    re-commit after deleting obj files

---
+++ Day01/Program.fs
@@ -0,0 +1,28 @@
+﻿module Day01 =
+
+    let maxCals lines =
+        let folder (elfSum, maxCals) s =
+            match s with
+            | "" -> (0, maxCals)
+            | s  -> 
+                let runningTotal = elfSum + (int s)
+                (runningTotal, max runningTotal maxCals)
+        
+        lines |> Array.fold folder (0, 0) |> snd
+
+    // read in the values as array of string
+    let lines = System.IO.File.ReadAllLines(@"input.txt")
+    printfn "MaxCals = %i" (maxCals lines)
+
+    let topN n lines =
+        let folder2 (elfSum, elfSums) s =
+            match s with
+            | ""    -> (0, elfSum :: elfSums)
+            | s     -> (elfSum + (int s), elfSums)
+
+        let (finalSum, elfSums) = lines |> Array.fold folder2 (0, List.empty)
+        let elfSums = finalSum :: elfSums |> List.sort |> List.rev
+        elfSums |> List.take n |> List.sum
+
+    printfn "Top 3 sum = %i" (topN 3 lines) 
+
---
+++ Day03/Program.fs
@@ -0,0 +1,42 @@
+﻿module Day03 =
+    //let lines = System.IO.File.ReadAllLines(@"input.txt")
+    //let allChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray()
+
+    //let findPriority c = (if c >= 'a' then (int c - int 'a') else (int c - int 'A') + 26) + 1
+
+    //let mapper (line:string) =
+    //    let chars = line.ToCharArray()
+    //    let charsL = chars.[..(chars.Length/2-1)] |> Set.ofArray
+    //    let charsR = chars.[(chars.Length/2)..]   |> Set.ofArray
+    //    let dodgyChar =
+    //        let intersect = Set.intersect charsL charsR
+    //        if intersect.Count = 1 then intersect.MaximumElement else raise (System.ArgumentException("Dodgy input"))
+    //    findPriority dodgyChar
+
+    //let answer = lines |> Array.map mapper |> Array.sum
+
+    //printfn "Answer: %i" answer
+
+    let lines = System.IO.File.ReadAllLines(@"input.txt")
+    let findPriority c = (if c >= 'a' then (int c - int 'a') else (int c - int 'A') + 26) + 1
+    let p2answer =
+        lines
+        |> Array.map (fun s -> s.ToCharArray() |> Set.ofArray)
+        |> Array.chunkBySize 3
+        |> Array.map (fun group -> group |> Array.reduce (Set.intersect) |> Set.maxElement |> findPriority)
+        |> Array.sum
+
+    let p2answer' =
+        lines
+        |> Seq.map (fun s -> s.ToCharArray() |> Set.ofArray)
+        |> Seq.chunkBySize 3
+        |> Seq.collect Set.intersectMany
+        |> Seq.map findPriority
+        |> Seq.sum
+
+
+    printfn "Part 2: %i" p2answer
+    printfn "Part 2: %i" p2answer'
+
+
+    
