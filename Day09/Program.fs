d5846c8309c8a4cee11c2879d67c1dfc94a63ae6
Paul Papathomas <paul@pauls-mbp.home>
2022-12-10 08:46:12 +0000

    Day09, Part 1 initial commit

---
+++ Day09/Program.fs
@@ -0,0 +1,32 @@
+﻿module Day09 =
+    let lines = System.IO.File.ReadAllLines(@"input.txt")
+    let moves =
+        lines
+        |> Seq.map (fun s ->
+                        let parts = s.Split(' ', System.StringSplitOptions.RemoveEmptyEntries)
+                        parts.[0], int parts.[1])
+
+    let getNewTailPosition (xH, yH) (xT, yT) =
+        match xH-xT, yH-yT with
+        | 0, 0 -> (xT, yT)
+        | dx, 0 when abs dx > 1 -> (xT + 1 * sign(dx), yT)
+        | 0, dy when abs dy > 1 -> (xT, yT + 1 * sign(dy))
+        | dx, dy when abs dx > 1 || abs dy > 1 -> (xT + 1 * sign(dx), yT + 1 * sign(dy))
+        | _ -> (xT, yT)
+
+    let moveFolder (head, tail, tailXYs) (dir, steps) =
+        let dx, dy =
+            match dir with
+            | "R" -> 1, 0
+            | "L" -> -1, 0
+            | "U" -> 0, 1
+            | "D" -> 0, -1
+        let stepFolder ((xH, yH), tail, tailXYs) _ =
+            let headNew = xH + dx, yH + dy
+            let tailNew = getNewTailPosition headNew tail
+            headNew, tailNew, tailXYs |> Set.add tailNew
+        [1..steps] |> Seq.fold stepFolder (head, tail, tailXYs)
+
+    let head, tail, tailXYs = moves |> Seq.fold moveFolder ((0,0), (0,0), Set.empty |> Set.add (0,0))
+
+    printfn "Part 1, answer: %i" tailXYs.Count
