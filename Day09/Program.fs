1192b25eb1fb9ecb628e954c01db5a4b252f1cdf
Paul Papathomas <paul@unknown.home>
2022-12-10 19:35:39 +0000

    Day 9, Part 2 comit

--- Day09/Program.fs
+++ Day09/Program.fs
@@ -10,19 +10,32 @@
         | dx, dy when abs dx > 1 || abs dy > 1 -> (xT + 1 * sign(dx), yT + 1 * sign(dy))
         | _ -> (xT, yT)
 
-    let moveFolder (head, tail, tailXYs) (dir, steps) =
+    let moveHead dir (x, y) =
         let dx, dy =
             match dir with
             | "R" -> 1, 0
             | "L" -> -1, 0
             | "U" -> 0, 1
             | "D" -> 0, -1
-        let stepFolder ((xH, yH), tail, tailXYs) _ =
-            let headNew = xH + dx, yH + dy
-            let tailNew = getNewTailPosition headNew tail
-            headNew, tailNew, tailXYs |> Set.add tailNew
-        [1..steps] |> Seq.fold stepFolder (head, tail, tailXYs)
+        x + dx, y + dy
 
-    let head, tail, tailXYs = moves |> Seq.fold moveFolder ((0,0), (0,0), Set.empty |> Set.add (0,0))
+    let moveFolder (head, tail, tailHistory) (dir, steps) =
+        let tailFolder (head, tail) t =
+            let tNew = getNewTailPosition head t
+            (tNew, tNew::tail)
 
-    printfn "Part 1, answer: %i" tailXYs.Count
+        let stepFolder (head, tail, tailHistory) _ =
+            let headNew = moveHead dir head
+            let (tLast, tail) = tail |> List.fold tailFolder (headNew, List.empty)
+            headNew, tail |> List.rev, tailHistory |> Set.add tLast
+
+        [1..steps] |> Seq.fold stepFolder (head, tail, tailHistory)
+
+    let findSolution tailLength =
+        moves |> Seq.fold moveFolder ((0,0), List.replicate tailLength (0,0), Set.empty |> Set.add (0,0))
+
+    let _, _, tailHistoryP1 = findSolution 1
+    printfn "Part 1, answer: %i" tailHistoryP1.Count
+
+    let _, _, tailHistoryP2 = findSolution 9
+    printfn "Part 2, answer: %i" tailHistoryP2.Count
