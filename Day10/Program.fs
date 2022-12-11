142309c91c3d77709271d4ba4ecff47ae298c459
Paul Papathomas <paul@Pauls-MacBook-Pro.local>
2022-12-11 13:19:55 +0000

    Part 2, first version

--- Day10/Program.fs
+++ Day10/Program.fs
@@ -20,7 +20,7 @@ module Day10 =
     let commands = lines |> List.map mapper
 
     let mutable x = 1
-    let folder ((currentCommand, cyclesLeft), commandsLeft, signalStrength) cycle =
+    let p1folder ((currentCommand, cyclesLeft), commandsLeft, signalStrength) cycle =
         let signalStrength = 
             if (cycle - 20)%40 = 0 then
                 signalStrength + cycle*x
@@ -45,7 +45,7 @@ module Day10 =
         
         c, commandsLeft, signalStrength
 
-    let getSolution() =
+    let getSolutionP1() =
         let h = commands |> List.head
         let t = commands |> List.tail
 
@@ -53,7 +53,46 @@ module Day10 =
                 | Addx(c, _) -> (h, c)
                 | Noop(c)    -> (h, c)
 
-        let _, _, signalStrength = [|1..220|] |> Array.fold folder (c, t, 0)
+        let _, _, signalStrength = [|1..220|] |> Array.fold p1folder (c, t, 0)
         signalStrength
 
-    printfn "Part1: %i" (getSolution())
+    printfn "Part1: %i" (getSolutionP1())
+
+    x <- 1
+    let p2folder ((currentCommand, cyclesLeft), commandsLeft, pixels) cycle =
+        let pixel = if abs(x-(cycle-1)%40) > 1 then " " else "#"
+
+        let cyclesLeft = cyclesLeft - 1
+        let c, commandsLeft = 
+            if cyclesLeft = 0 then
+                // execute command
+                match currentCommand with
+                | Addx(_, v)    -> x <- x + v
+                | Noop(_)       -> ()
+
+                match commandsLeft with
+                | h :: t -> match h with
+                            | Addx(c, _) -> (h, c), t
+                            | Noop(c)    -> (h, c), t
+                | _ -> (Noop(1), 1), List.empty
+            else
+                (currentCommand, cyclesLeft), commandsLeft
+        
+        c, commandsLeft, pixel::pixels
+
+    let getSolutionP2() =
+        let h = commands |> List.head
+        let t = commands |> List.tail
+
+        let c = match h with
+                | Addx(c, _) -> (h, c)
+                | Noop(c)    -> (h, c)
+
+        let _, _, pixels = [|1..240|] |> Array.fold p2folder (c, t, List.empty)
+        pixels
+        |> List.rev
+        |> List.chunkBySize 40
+        |> List.map (fun el -> el |> String.concat "")
+        |> List.iter (printfn "%s")
+
+    getSolutionP2() 
