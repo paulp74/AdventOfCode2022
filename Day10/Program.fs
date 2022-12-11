8655907b5f6c76f2f5eaf1620a4bdb700f72ccdb
Paul Papathomas <paul@pauls-mbp.home>
2022-12-11 18:08:26 +0000

    Make the folder generic

--- Day10/Program.fs
+++ Day10/Program.fs
@@ -1,6 +1,7 @@
 ﻿open System.Text.RegularExpressions
 
 module Day10 =
+    // Read in input and generate commands
     let (|Regex|_|) pattern input =
         let m = Regex.Match(input, pattern)
         if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
@@ -20,75 +21,58 @@ module Day10 =
     let commands = lines |> List.map mapper
 
     let mutable x = 1
-    let p1CycleFolder ((currentCommand, commandCycles), commandsLeft, signalStrength) cycle =
-        let signalStrength = 
+    let genericCycleFolder f ((currentCommand, commandCycles), commandsLeft, argF) cycle =
+        let outF = f cycle x argF // act upon value of X to update acc
+
+        let commandCycles = commandCycles + 1
+        let c, commandsLeft = 
+            let getNext() =
+                match commandsLeft with
+                | h :: t -> (h, 0), t
+                | _ -> (Noop(1), 1), List.empty
+
+            match currentCommand with
+            | Addx(c, v) when c = commandCycles ->
+                x <- x + v
+                getNext()
+            | Noop(c)    when c = commandCycles ->
+                ()
+                getNext()
+            | _ ->
+                (currentCommand, commandCycles), commandsLeft
+        c, commandsLeft, outF
+
+    // Part 1
+    x <- 1
+    let signalStrength' cycle x signalStrength =
             if (cycle - 20)%40 = 0 then
                 signalStrength + cycle*x
             else
                 signalStrength
-
-        let commandCycles = commandCycles + 1
-        let c, commandsLeft = 
-            let getNext() =
-                match commandsLeft with
-                | h :: t -> (h, 0), t
-                | _ -> (Noop(1), 1), List.empty
-
-            match currentCommand with
-            | Addx(c, v) when c = commandCycles ->
-                x <- x + v
-                getNext()
-            | Noop(c)    when c = commandCycles ->
-                ()
-                getNext()
-            | _ ->
-                (currentCommand, commandCycles), commandsLeft
-
-        c, commandsLeft, signalStrength
-
-    let getSolutionP1() =
+    let p1CycleFolder' = genericCycleFolder signalStrength'
+    let getSolutionP1'() =
         match commands with
         | h::t ->
-            let _, _, signalStrength = [|1..220|] |> Array.fold p1CycleFolder ((h, 0), t, 0)
+            let _, _, signalStrength = [|1..220|] |> Array.fold p1CycleFolder' ((h, 0), t, 0)
             signalStrength
         | _    ->
             raise(System.ArgumentException("Expected more commands"))
+    printfn "Part1: %i" (getSolutionP1'())
 
-    printfn "Part1: %i" (getSolutionP1())
-
+    // Part 2
     x <- 1
-    let p2folder ((currentCommand, commandCycles), commandsLeft, pixels) cycle =
-        let pixel = if abs(x-(cycle-1)%40) > 1 then " " else "#"
+    let pixels' cycle x pixels = (if abs(x-(cycle-1)%40) > 1 then " " else "#") :: pixels
+    let p2CycleFolder' = genericCycleFolder pixels'
 
-        let commandCycles = commandCycles + 1
-        let c, commandsLeft = 
-            let getNext() =
-                match commandsLeft with
-                | h :: t -> (h, 0), t
-                | _ -> (Noop(1), 1), List.empty
+    match commands with
+    | h::t ->
+        let _, _, pixels = [|1..240|] |> Array.fold p2CycleFolder' ((h, 0), t, List.empty)
+        pixels
+        |> List.rev
+        |> List.chunkBySize 40
+        |> List.map (fun el -> el |> String.concat "")
+        |> List.iter (printfn "%s")
+    | _    ->
+        raise(System.ArgumentException("Expected more commands"))
 
-            match currentCommand with
-            | Addx(c, v) when c = commandCycles ->
-                x <- x + v
-                getNext()
-            | Noop(c)    when c = commandCycles ->
-                ()
-                getNext()
-            | _ ->
-                (currentCommand, commandCycles), commandsLeft
-        
-        c, commandsLeft, pixel::pixels
-
-    let getSolutionP2() =
-        match commands with
-        | h::t ->
-            let _, _, pixels = [|1..240|] |> Array.fold p2folder ((h, 0), t, List.empty)
-            pixels
-            |> List.rev
-            |> List.chunkBySize 40
-            |> List.map (fun el -> el |> String.concat "")
-            |> List.iter (printfn "%s")
-        | _    ->
-            raise(System.ArgumentException("Expected more commands"))
-
-    getSolutionP2() 
+   
